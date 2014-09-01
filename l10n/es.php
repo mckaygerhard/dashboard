019f0dc85f0d29d6e4539fe2d9878446ba662ae1
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-09-01 02:35:09 -0400

    [tx-robot] updated from transifex

---
+++ l10n/es.php
@@ -0,0 +1,22 @@
+<?php
+$TRANSLATIONS = array(
+"Dashboard" => "Escritorio",
+"Welcome to your personal dashboard" => "Bienvenido a su escritorio personal",
+"This screen is shown, because no widgets are activated. Go to your personal settings and activate some. You can also set some configurations in there." => "Esta pantalla se muestra dado que no hay widgets activados. Vaya a sus ajustes personales y active algunos. Tambien puede encontrar configuraciones alli",
+"Some more" => "Algo mas",
+"Dashboard Settings" => "Configuración del Escritorio",
+"Yesterday" => "Ayer",
+"Today" => "Hoy",
+"Tomorrow" => "Mañana",
+"Connection error. <br />Are the settings correct?" => "Error de conexión. <br />Esta configurado correctamente?",
+"From" => "Desde",
+"About" => "Acerca de",
+"There are %d new mails..." => "Hay %d nuevos correos electronicos",
+"Mark as read." => "Marcar como leido",
+"Mark as done." => "Marcar como finalizado",
+"Temperature" => "Temperatura",
+"Windspeed" => "Velocidad del viento",
+"Humidity" => "Humedad",
+"Pressure" => "Presión"
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";

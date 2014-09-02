37bcadd527d97314c8db7726a2fec919d82c2ef4
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-09-02 02:35:08 -0400

    [tx-robot] updated from transifex

---
+++ l10n/bn_BD.php
@@ -0,0 +1,28 @@
+<?php
+$TRANSLATIONS = array(
+"Dashboard" => "ড্যাশবোর্ড",
+"Welcome to your personal dashboard" => "আপনার ব্যক্তিগত ড্যাশবোর্ডে সুস্বাগতম",
+"This screen is shown, because no widgets are activated. Go to your personal settings and activate some. You can also set some configurations in there." => "কোন উইজেট চালু না করায় এই স্ক্রিন দেখছেন। ব্যক্তিগত নিয়ামক-এ যান এবং কিছু সক্রিয় করুন। ওখানে কিছু কনফিগারেশন ও নির্ধারণ করতে পারেন।",
+"You are admin, so you can edit generel settings:" => "আপনি প্রশাসক, সূতরাং আপনি সাধারণ নিয়ামক সম্পাদনা করতে পারেন:",
+"Set if non-commercial widgets are not allowed" => "অবাণিজ্যিক উইজেটকে অনুমতি না দিলে নির্ধারণ করুন",
+"Edit the availible widgets" => "প্রাপ্তব্য উইজেট সম্পাদনা কর",
+"Some more" => "আরো কিছু",
+"Go to your personal settings" => "আপনার নিজস্ব নিয়ামকে যান",
+"Go to admin settings" => "প্রশাসকের নিয়ামকে যান",
+"Dashboard Settings" => "ড্যাশবোর্ড নিয়ামক",
+"<p style=\"padding-top: 20px;\">There are some background colors for the widgets.<ul><li>red: Error</li><li>orange: missconfiguration</li><li>green: there is something new</li><li></li></ul></p>" => "<p style=\"padding-top: 20px;\">উইজেটসমূহের জন্য কিছু ব্যাকগ্রাউন্ড রং আছে।<ul><li>red: Error</li><li>orange: missconfiguration</li><li>green: there is something new</li><li></li></ul></p>",
+"Yesterday" => "গতকাল",
+"Today" => "আজ",
+"Tomorrow" => "আগামীকাল",
+"Connection error. <br />Are the settings correct?" => "সংযোগ সমস্যা: <br />নিয়ামকসমূহ ঠিক আছে? ",
+"From" => "শুরুর সময়",
+"About" => "সমপর্কে",
+"There are %d new mails..." => "%d নতুন মেইল আছে ...",
+"Mark as read." => "পঠিত হিসেবে চিহ্নিত কর।",
+"Mark as done." => "সম্পন্ন হিসেবে চিহ্নিত কর।",
+"Temperature" => "তাপমাত্রা",
+"Windspeed" => "বাতাসের গতি",
+"Humidity" => "আর্দ্রতা",
+"Pressure" => "বায়ুচাপ"
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
---
+++ l10n/de_DE.php
@@ -0,0 +1,28 @@
+<?php
+$TRANSLATIONS = array(
+"Dashboard" => "Schreibtisch",
+"Welcome to your personal dashboard" => "Herzlich Willkommen auf Ihrem persönlichen Schreibtisch",
+"This screen is shown, because no widgets are activated. Go to your personal settings and activate some. You can also set some configurations in there." => "Diese Seite wird angezeigt, da keine Widgets aktiviert sind. Wechseln Sie zu Ihren persönlichen Einstellungen und aktivieren Sie dort einige. Sie können dort auch einige Einstellungen treffen.",
+"You are admin, so you can edit generel settings:" => "Sie sind Administrator, somit können Sie die allgemeinen Einstellungen bearbeiten:",
+"Set if non-commercial widgets are not allowed" => "Aktivieren, falls nicht-kommerzielle Widgets nicht erlaubt sind",
+"Edit the availible widgets" => "Verfügbare Widgets bearbeiten",
+"Some more" => "Einiges mehr",
+"Go to your personal settings" => "Zu Ihren persönlichen Einstellungen wechseln",
+"Go to admin settings" => "Zu den Administrationseinstellungen wechseln",
+"Dashboard Settings" => "Schreibtisch-Einstellungen",
+"<p style=\"padding-top: 20px;\">There are some background colors for the widgets.<ul><li>red: Error</li><li>orange: missconfiguration</li><li>green: there is something new</li><li></li></ul></p>" => "<p style=\"padding-top: 20px;\">Es gibt einige Hintergrundfarben für die Widgets.<ul><li>Rot: Fehler</li><li>Orange: Fehlerhafte Konfiguration</li><li>Grün: Irgendetwas ist neu</li><li></li></ul></p>",
+"Yesterday" => "Gestern",
+"Today" => "Heute",
+"Tomorrow" => "Morgen",
+"Connection error. <br />Are the settings correct?" => "Verbindungsfehler.<br/>Sind die Einstellungen korrekt?",
+"From" => "von",
+"About" => "Über",
+"There are %d new mails..." => "%d neue E-Mails vorhanden...",
+"Mark as read." => "Als gelesen markieren.",
+"Mark as done." => "Als erledigt markieren.",
+"Temperature" => "Temperatur",
+"Windspeed" => "Windgeschwindigkeit",
+"Humidity" => "Luftfeuchtigkeit",
+"Pressure" => "Luftdruck"
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";

a3ac7da085007f8ef0c2c6fca278f474d5ffa046
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-09-03 02:35:10 -0400

    [tx-robot] updated from transifex

---
+++ l10n/da.php
@@ -0,0 +1,28 @@
+<?php
+$TRANSLATIONS = array(
+"Dashboard" => "Instrumentbræt",
+"Welcome to your personal dashboard" => "Velkommen til dit personlige instrumentbræt",
+"This screen is shown, because no widgets are activated. Go to your personal settings and activate some. You can also set some configurations in there." => "Denne side vises, da der ikke er slået kontroller til. Gå til dine personlige indstillinger og slå nogle til. Du kan også angive en række konfigurationer derinde.",
+"You are admin, so you can edit generel settings:" => "Du er admin, så du kan redigere generelle indstillinger:",
+"Set if non-commercial widgets are not allowed" => "Angiv om ikke-kommercielle kontroller ikke skal tillades",
+"Edit the availible widgets" => "Redigér de tilgængelige kontroller",
+"Some more" => "Endnu flere",
+"Go to your personal settings" => "Gå til dine personlige indstillinger",
+"Go to admin settings" => "Gå til admin-indstillinger",
+"Dashboard Settings" => "Indstillinger for instrumentbræt",
+"<p style=\"padding-top: 20px;\">There are some background colors for the widgets.<ul><li>red: Error</li><li>orange: missconfiguration</li><li>green: there is something new</li><li></li></ul></p>" => "<p style=\"padding-top: 20px;\">Der er en række baggrundsfarver til kontrollerne.<ul><li>rød: Fejl</li><li>orange: forkert konfiguration</li><li>grøn: der er noget nyt</li><li></li></ul></p>",
+"Yesterday" => "I går",
+"Today" => "I dag",
+"Tomorrow" => "I morgen",
+"Connection error. <br />Are the settings correct?" => "Tilslutningsfejl. <br />Er indstillingerne korrekte?",
+"From" => "Fra",
+"About" => "Om",
+"There are %d new mails..." => "Der er %d nye beskeder...",
+"Mark as read." => "Markér som læst",
+"Mark as done." => "Markér som fuldført.",
+"Temperature" => "Temperatur",
+"Windspeed" => "Vindhastighed",
+"Humidity" => "Luftfugtighed",
+"Pressure" => "Lufttryk"
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
---
+++ l10n/sl.php
@@ -0,0 +1,28 @@
+<?php
+$TRANSLATIONS = array(
+"Dashboard" => "Nadzorna plošča",
+"Welcome to your personal dashboard" => "Dobrodošli v nastavitvah nadzorne plošče",
+"This screen is shown, because no widgets are activated. Go to your personal settings and activate some. You can also set some configurations in there." => "Ta zaslon je prikazan, ker ni omogočenih gradnikov. Te lahko spreminjate in na nastavljate med osebnimi nastavitvami.",
+"You are admin, so you can edit generel settings:" => "Omogočena so skrbniška dovoljenja, zato je dovoljeno urejati splošne nastavitve:",
+"Set if non-commercial widgets are not allowed" => "Nastavi, če so dovoljeni ne-plačniški gradniki",
+"Edit the availible widgets" => "Uredi razpoložljive gradnike",
+"Some more" => "Še več",
+"Go to your personal settings" => "Osebne nastavitve",
+"Go to admin settings" => "Skrbniške nastavitve",
+"Dashboard Settings" => "Nastavitve nadzorne plošče",
+"<p style=\"padding-top: 20px;\">There are some background colors for the widgets.<ul><li>red: Error</li><li>orange: missconfiguration</li><li>green: there is something new</li><li></li></ul></p>" => "<p style=\"padding-top: 20px;\">Za gradnike so omogočene barve ozadja.<ul><li>rdeča: napaka</li><li>oranžna: napačna nastavitev</li><li>zelena: dodana je nova zmožnost</li><li></li></ul></p>",
+"Yesterday" => "Včeraj",
+"Today" => "Danes",
+"Tomorrow" => "Jutri",
+"Connection error. <br />Are the settings correct?" => "Napaka povezave.<br />Ali so nastavitve pravilne?",
+"From" => "Od",
+"About" => "&O programu...",
+"There are %d new mails..." => "Nova sporočila: %d",
+"Mark as read." => "Označi kot prebrano",
+"Mark as done." => "Označi kot končano",
+"Temperature" => "Temperatura",
+"Windspeed" => "Hitrost vetra",
+"Humidity" => "Zračna vlaga",
+"Pressure" => "Zračni tlak"
+);
+$PLURAL_FORMS = "nplurals=4; plural=(n%100==1 ? 0 : n%100==2 ? 1 : n%100==3 || n%100==4 ? 2 : 3);";

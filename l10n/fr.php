c4e27a697e63e56bfcc486b4918d3fca34e91788
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-09-09 02:35:09 -0400

    [tx-robot] updated from transifex

---
+++ l10n/fr.php
@@ -0,0 +1,26 @@
+<?php
+$TRANSLATIONS = array(
+"Dashboard" => "Tableau de bord",
+"Welcome to your personal dashboard" => "Bienvenue sur votre tableau de bord personnel",
+"This screen is shown, because no widgets are activated. Go to your personal settings and activate some. You can also set some configurations in there." => "Ce message apparaît car aucun widget n'est activé. Allez dans vos paramètres personnels pour en activer. Vous pouvez également y ajuster quelques configurations.",
+"You are admin, so you can edit generel settings:" => "Vous êtes admin, vous pouvez donc éditer les paramètres généraux :",
+"Edit the availible widgets" => "Éditer les widgets disponibles",
+"Some more" => "Davantage",
+"Go to your personal settings" => "Allez dans vos paramètres personnels",
+"Go to admin settings" => "Allez dans les paramètres admin",
+"Dashboard Settings" => "Paramètres du Tableau de bord",
+"Yesterday" => "Hier",
+"Today" => "Aujourd'hui",
+"Tomorrow" => "Demain",
+"Connection error. <br />Are the settings correct?" => "Erreur de connection. <br />Les paramètres sont-ils corrects ?",
+"From" => "De",
+"About" => "À propos de",
+"There are %d new mails..." => "Il y a %d nouveaux courriels...",
+"Mark as read." => "Marquer comme lu.",
+"Mark as done." => "Marquer comme terminé.",
+"Temperature" => "Température",
+"Windspeed" => "Vitesse du vent",
+"Humidity" => "Humidité",
+"Pressure" => "Pression"
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n > 1);";

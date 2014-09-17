95d428fa9c84eb6d8675621521f7e0cbb31a29cc
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-09-17 02:35:11 -0400

    [tx-robot] updated from transifex

---
+++ l10n/pl.php
@@ -0,0 +1,28 @@
+<?php
+$TRANSLATIONS = array(
+"Dashboard" => "Panel sterowania",
+"Welcome to your personal dashboard" => "Witamy w osobistym panelu sterowania",
+"This screen is shown, because no widgets are activated. Go to your personal settings and activate some. You can also set some configurations in there." => "Ten ekran pojawia się, ponieważ nie są aktywowane żadne widgety. Idź do ustawień osobistych i aktywuj kilka z nich. Możesz tam również ustawić konfigurację.",
+"You are admin, so you can edit generel settings:" => "Jesteś adminem, więc możesz edytować ustawienia ogólne:",
+"Set if non-commercial widgets are not allowed" => "Ustaw, jeśli niekomercyjne widgety nie mają być dozwolone",
+"Edit the availible widgets" => "Edytuj dostępne widgety",
+"Some more" => "Trochę wiecej",
+"Go to your personal settings" => "Idź do ustawień osobistych",
+"Go to admin settings" => "Idź do ustawień administracyjnych",
+"Dashboard Settings" => "Ustawienia Panelu Sterowania",
+"<p style=\"padding-top: 20px;\">There are some background colors for the widgets.<ul><li>red: Error</li><li>orange: missconfiguration</li><li>green: there is something new</li><li></li></ul></p>" => "<p style=\"padding-top: 20px;\">Następujące kolory tła oznaczają w widgetach.<ul><li>czerwony: Błąd</li><li>pomarańcz: błąd konfiguracji</li><li>zielony: coś nowego</li><li></li></ul></p>",
+"Yesterday" => "Wczoraj",
+"Today" => "Dziś",
+"Tomorrow" => "Jutro",
+"Connection error. <br />Are the settings correct?" => "Błąd połączenia. <br />Czy ustawienia są poprawne?",
+"From" => "Od",
+"About" => "O aplikacji",
+"There are %d new mails..." => "Jest %d nowych wiadomości...",
+"Mark as read." => "Oznacz jako przeczytane.",
+"Mark as done." => "Oznacz jako zrobione.",
+"Temperature" => "Temperatura",
+"Windspeed" => "Prędkość wiatru",
+"Humidity" => "Wilgotność",
+"Pressure" => "Ciśnienie"
+);
+$PLURAL_FORMS = "nplurals=3; plural=(n==1 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);";

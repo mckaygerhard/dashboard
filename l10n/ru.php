ff790903d52fde202a2a37aeececf0834c40a196
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-09-19 02:35:10 -0400

    [tx-robot] updated from transifex

---
+++ l10n/ru.php
@@ -0,0 +1,24 @@
+<?php
+$TRANSLATIONS = array(
+"Dashboard" => "Панель",
+"Welcome to your personal dashboard" => "Добро пожаловать в панель управления",
+"This screen is shown, because no widgets are activated. Go to your personal settings and activate some. You can also set some configurations in there." => "Эта страница отображена, поскольку не активирован ни один виджет. Перейдите на страницу личных предпочтений и включите хотя бы один. Там же вы сможете управлять и другими элементами конфигурации.",
+"You are admin, so you can edit generel settings:" => "Вы администратор, и можете отредактировать основные настройки:",
+"Some more" => "Ещё",
+"Go to admin settings" => "Перейти к настройкам",
+"Dashboard Settings" => "Настройки панели",
+"<p style=\"padding-top: 20px;\">There are some background colors for the widgets.<ul><li>red: Error</li><li>orange: missconfiguration</li><li>green: there is something new</li><li></li></ul></p>" => "<p style=\"padding-top: 20px;\">Для виджетов применимы такие цвета фона:<ul><li>красный: ошибка</li><li>оранжевый: неверная конфигурация</li><li>зеленый: есть что-то новое</li><li></li></ul></p>",
+"Yesterday" => "Вчера",
+"Today" => "Сегодня",
+"Tomorrow" => "Завтра",
+"Connection error. <br />Are the settings correct?" => "Ошибка подключения. <br />Верны ли настройки?",
+"From" => "С",
+"About" => "О программе",
+"There are %d new mails..." => "Новых сообщений: %d",
+"Mark as read." => "Пометить как прочтённые.",
+"Temperature" => "Температура",
+"Windspeed" => "Скорость ветра",
+"Humidity" => "Влажность",
+"Pressure" => "Давление"
+);
+$PLURAL_FORMS = "nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);";
---
+++ l10n/sk_SK.php
@@ -0,0 +1,28 @@
+<?php
+$TRANSLATIONS = array(
+"Dashboard" => "Nástenka",
+"Welcome to your personal dashboard" => "Vitajte na vašej nástenke",
+"This screen is shown, because no widgets are activated. Go to your personal settings and activate some. You can also set some configurations in there." => "Túto obrazovku vidíte len ak nie sú aktivované žiadne widgety. Prejdite do osobného nastavenia a nejaké si aktivujte. Môžete si tam tiež upraviť niektoré vlastné nastavenia.",
+"You are admin, so you can edit generel settings:" => "Ste administrátorom, preto môžete zmeniť základné nastavenia:",
+"Set if non-commercial widgets are not allowed" => "Nastavte pre povolenie nekomerčných widgetov",
+"Edit the availible widgets" => "Upraviť dostupné widgety",
+"Some more" => "Niekoľko ďalších",
+"Go to your personal settings" => "Prejsť do osobného nastavenia",
+"Go to admin settings" => "Prejsť do administrátorských nastavení",
+"Dashboard Settings" => "Nastavenia nástenky",
+"<p style=\"padding-top: 20px;\">There are some background colors for the widgets.<ul><li>red: Error</li><li>orange: missconfiguration</li><li>green: there is something new</li><li></li></ul></p>" => "<p style=\"padding-top: 20px;\">Pre pozadie widgetov sú použité niektoré farby.<ul><li>červená: Chyba</li><li>oranžová: nesprávna konfigurácia</li><li>zelená: pribudlo niečo nové</li><li></li></ul></p>",
+"Yesterday" => "Včera",
+"Today" => "Dnes",
+"Tomorrow" => "Zajtra",
+"Connection error. <br />Are the settings correct?" => "Chyba spojenia. <br />Máte správne nastavenia?",
+"From" => "Od",
+"About" => "O",
+"There are %d new mails..." => "Máte %d nových emailov...",
+"Mark as read." => "Označiť ako prečítané.",
+"Mark as done." => "Označiť ako dokončené.",
+"Temperature" => "Teplota",
+"Windspeed" => "Rýchlosť vetra",
+"Humidity" => "Vlhkosť",
+"Pressure" => "Tlak"
+);
+$PLURAL_FORMS = "nplurals=3; plural=(n==1) ? 0 : (n>=2 && n<=4) ? 1 : 2;";

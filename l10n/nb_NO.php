a80c863bb5168eb8d4ea1873d20c551c330d1459
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-09-29 02:35:11 -0400

    [tx-robot] updated from transifex

---
+++ l10n/nb_NO.php
@@ -0,0 +1,28 @@
+<?php
+$TRANSLATIONS = array(
+"Dashboard" => "Dashbord",
+"Welcome to your personal dashboard" => "Velkommen til ditt eget dashbord",
+"This screen is shown, because no widgets are activated. Go to your personal settings and activate some. You can also set some configurations in there." => "Denne skjermen vises fordi ingen dashbord-elementer er aktivert. Gå til innstillengen dine og aktiver noen. Du kan også gjøre noe konfigurasjon der.",
+"You are admin, so you can edit generel settings:" => "Du er administrator, så du kan redigere standardinnstillinger:",
+"Set if non-commercial widgets are not allowed" => "Sett om ikke-kommersielle dashbord-elementer ikke tillates",
+"Edit the availible widgets" => "Rediger tilgjengelige dashbord-elementer",
+"Some more" => "Litt mer",
+"Go to your personal settings" => "Gå til dine innstillinger",
+"Go to admin settings" => "Gå til admin-innstillinger",
+"Dashboard Settings" => "Dashbord-innstillinger",
+"<p style=\"padding-top: 20px;\">There are some background colors for the widgets.<ul><li>red: Error</li><li>orange: missconfiguration</li><li>green: there is something new</li><li></li></ul></p>" => "<p style=\"padding-top: 20px;\">Dashbord-elementene har  bakgrunnsfarger.<ul><li>rød: Feil</li><li>oransje: feilkonfigurering</li><li>grønn: noe er nytt</li><li></li></ul></p>",
+"Yesterday" => "I går",
+"Today" => "I dag",
+"Tomorrow" => "I morgen",
+"Connection error. <br />Are the settings correct?" => "Feil ved oppkobling. <br />Er innstillingene riktige?",
+"From" => "Fra",
+"About" => "Om",
+"There are %d new mails..." => "Der er %d nye e-poster...",
+"Mark as read." => "Merk som lest.",
+"Mark as done." => "Merk som utført.",
+"Temperature" => "Temperatur",
+"Windspeed" => "Vindhastighet",
+"Humidity" => "Fuktighet",
+"Pressure" => "Trykk"
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";

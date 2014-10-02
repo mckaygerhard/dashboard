632ad0fbb9a58a460ddbae197620ee75d7bdd196
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-10-02 02:35:12 -0400

    [tx-robot] updated from transifex

--- l10n/el.php
+++ l10n/el.php
@@ -2,7 +2,9 @@
 $TRANSLATIONS = array(
 "Dashboard" => "Πίνακας Ελέγχου",
 "Welcome to your personal dashboard" => "Καλώς ήλθατε στον προσωπικό σας πίνακα ελέγχου",
+"This screen is shown, because no widgets are activated. Go to your personal settings and activate some. You can also set some configurations in there." => "Αυτή η οθόνη εμφανίζεται διότι δεν είναι ενεργοποιημένο κανένα γραφικό συστατικό. Ανοίξτε τις προσωπικές ρυθμίσεις και ενεργοποιήστε μερκά. Εκεί μπορείτε επίσης να ορίσετε και κάποιες επιλογές.",
 "You are admin, so you can edit generel settings:" => "Είστε διαχειριστής, μπορείτε λοιπόν να επεξεργαστείτε γενικές ρυθμίσεις:",
+"Edit the availible widgets" => "Επεξεργασία των διαθέσιμων γραφικών συστατικά",
 "Some more" => "Περισσότερα",
 "Go to your personal settings" => "Μετάβαση στις προσωπικές ρυθμίσεις",
 "Go to admin settings" => "Μετάβαση στις ρυθμίσεις διαχειριστή",

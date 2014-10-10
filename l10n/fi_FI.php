82fec8964efad12b29e4b936d62d1dd767eabb00
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-10-10 02:35:11 -0400

    [tx-robot] updated from transifex

---
+++ l10n/fi_FI.php
@@ -0,0 +1,29 @@
+<?php
+$TRANSLATIONS = array(
+"Yesterday" => "Eilen",
+"Today" => "Tänään",
+"Tomorrow" => "Huomenna",
+"Could not fetch data for \"%s\".<br>Please try another value.<br><a href='%s'>&raquo;&nbsp;settings</a>" => "Tietoja kohteelle \"%s\" ei voitu hakea.<br>Yritä syöttää jokin toinen arvo.<br><a href='%s'>&raquo;&nbsp;asetukset</a>",
+"Dashboard Settings" => "Oman näkymän asetukset",
+"<p style=\"padding-top: 20px;\">There are some background colors for the widgets.<ul><li>red: Error</li><li>orange: missconfiguration</li><li>green: there is something new</li><li></li></ul></p>" => "<p style=\"padding-top: 20px;\">Widgeteille on joitain taustavärejä.<ul><li>punainen: virhe</li><li>oranssi: väärät asetukset</li><li>virhe: jotain uutta saatavilla</li><li></li></ul></p>",
+"From" => "Alkaa",
+"About" => "Tietoja",
+"There are %d new mails..." => "%d uutta sähköpostia...",
+"today" => "tänään",
+"tomorrow" => "huomenna",
+"in two days" => "kahden päivän päästä",
+"Temperature" => "Lämpötila",
+"day" => "päivä",
+"min" => "väh.",
+"max" => "eni.",
+"night" => "yö",
+"Wind" => "Tuuli",
+"Speed" => "Nopeus",
+"Direction" => "Suunta",
+"Cloudiness" => "Pilvisyys",
+"Pressure" => "Paine",
+"Humidity" => "Kosteus",
+"Search" => "Etsi",
+"Windspeed" => "Tuulen nopeus"
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
---
+++ l10n/ru.php
@@ -0,0 +1,29 @@
+<?php
+$TRANSLATIONS = array(
+"Yesterday" => "Вчера",
+"Today" => "Сегодня",
+"Tomorrow" => "Завтра",
+"Could not fetch data for \"%s\".<br>Please try another value.<br><a href='%s'>&raquo;&nbsp;settings</a>" => "Невозможно получить данные о \"%s\".<br>Пожалуйста, попробуйте другое значение.<br><a href='%s'>&raquo;&nbsp;settings</a>",
+"Dashboard Settings" => "Настройки панели",
+"<p style=\"padding-top: 20px;\">There are some background colors for the widgets.<ul><li>red: Error</li><li>orange: missconfiguration</li><li>green: there is something new</li><li></li></ul></p>" => "<p style=\"padding-top: 20px;\">Для виджетов применимы такие цвета фона:<ul><li>красный: ошибка</li><li>оранжевый: неверная конфигурация</li><li>зеленый: есть что-то новое</li><li></li></ul></p>",
+"From" => "С",
+"About" => "О программе",
+"There are %d new mails..." => "Новых сообщений: %d",
+"today" => "сегодня",
+"tomorrow" => "завтра",
+"in two days" => "за два дня",
+"Temperature" => "Температура",
+"day" => "Дни",
+"min" => "мин",
+"max" => "макс",
+"night" => "ночь",
+"Wind" => "Ветер",
+"Speed" => "Скорость",
+"Direction" => "Направление",
+"Cloudiness" => "Облачность",
+"Pressure" => "Давление",
+"Humidity" => "Влажность",
+"Search" => "Найти",
+"Windspeed" => "Скорость ветра"
+);
+$PLURAL_FORMS = "nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);";

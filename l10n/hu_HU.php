ca6dd2ce6cb2399b367f80fbcf6c191b8f3ae999
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-10-20 02:35:13 -0400

    [tx-robot] updated from transifex

---
+++ l10n/hu_HU.php
@@ -0,0 +1,26 @@
+<?php
+$TRANSLATIONS = array(
+"Yesterday" => "Tegnap",
+"Today" => "Ma",
+"Tomorrow" => "Holnap",
+"From" => "Ettől",
+"About" => "Rólunk",
+"There are %d new mails..." => "%d új üzenet van...",
+"today" => "ma",
+"tomorrow" => "holnap",
+"in two days" => "következő 2 napban",
+"Temperature" => "Hőmérséklet",
+"day" => "nap",
+"min" => "min.",
+"max" => "max.",
+"night" => "éjszaka",
+"Wind" => "Szél",
+"Speed" => "Sebesség",
+"Direction" => "Irány",
+"Cloudiness" => "Felhőzet",
+"Pressure" => "Nyomás",
+"Humidity" => "Páratartalom",
+"Search" => "Keresés",
+"Windspeed" => "Szélsebesség"
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";

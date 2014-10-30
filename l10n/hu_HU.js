292b5c6ffe88f6541d57f82163b967e027e39d70
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-10-30 06:57:43 -0400

    [tx-robot] updated from transifex

---
+++ l10n/hu_HU.js
@@ -0,0 +1,27 @@
+OC.L10N.register(
+    "ocdashboard",
+    {
+    "Yesterday" : "Tegnap",
+    "Today" : "Ma",
+    "Tomorrow" : "Holnap",
+    "From" : "Ettől",
+    "About" : "Rólunk",
+    "There are %d new mails..." : "%d új üzenet van...",
+    "today" : "ma",
+    "tomorrow" : "holnap",
+    "in two days" : "következő 2 napban",
+    "Temperature" : "Hőmérséklet",
+    "day" : "nap",
+    "min" : "min.",
+    "max" : "max.",
+    "night" : "éjszaka",
+    "Wind" : "Szél",
+    "Speed" : "Sebesség",
+    "Direction" : "Irány",
+    "Cloudiness" : "Felhőzet",
+    "Pressure" : "Nyomás",
+    "Humidity" : "Páratartalom",
+    "Search" : "Keresés",
+    "Windspeed" : "Szélsebesség"
+},
+"nplurals=2; plural=(n != 1);");
---
+++ l10n/ja.js
@@ -0,0 +1,27 @@
+OC.L10N.register(
+    "ocdashboard",
+    {
+    "Yesterday" : "昨日",
+    "Today" : "今日",
+    "Tomorrow" : "明日",
+    "Dashboard Settings" : "ダッシュボード設定",
+    "<p style=\"padding-top: 20px;\">There are some background colors for the widgets.<ul><li>red: Error</li><li>orange: missconfiguration</li><li>green: there is something new</li><li></li></ul></p>" : "<p style=\"padding-top: 20px;\">背景色ウィジットです。<ul><li>red: Error</li><li>orange: missconfiguration</li><li>green: there is something new</li><li></li></ul></p>",
+    "From" : "開始",
+    "About" : "について",
+    "There are %d new mails..." : "新着メールが %d 件あります。",
+    "today" : "今日",
+    "tomorrow" : "明日",
+    "in two days" : "2日以内",
+    "Temperature" : "気温",
+    "day" : "日",
+    "min" : "最低",
+    "max" : "最高",
+    "night" : "夜",
+    "Wind" : "風",
+    "Direction" : "向き",
+    "Pressure" : "気圧",
+    "Humidity" : "湿度",
+    "Search" : "検索",
+    "Windspeed" : "風速"
+},
+"nplurals=1; plural=0;");

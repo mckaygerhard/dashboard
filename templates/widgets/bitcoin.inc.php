7493d84a464d1ad27d089d06804e915c21f4376e
namelivia <ohcan2@gmail.com>
2014-11-20 18:47:57 +0100

    Bitcoin price widget, based on Bitstamp price

---
+++ templates/widgets/bitcoin.inc.php
@@ -0,0 +1,4 @@
+<?php 
+echo("<p id='last'>".$additionalparams["last"]." USD</p>"); 
+echo("<p id='low'>".$additionalparams["low"]." USD</p>"); 
+echo("<p id='high'>".$additionalparams["high"]." USD</p>"); ?>

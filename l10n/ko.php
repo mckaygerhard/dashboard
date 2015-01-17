a5fd19a5c070cc8e6c2be9f8add63b8ea6515b7f
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-01-17 02:35:14 -0500

    [tx-robot] updated from transifex

---
+++ l10n/ko.php
@@ -0,0 +1,29 @@
+<?php
+$TRANSLATIONS = array(
+"Yesterday" => "어제",
+"Today" => "오늘",
+"Tomorrow" => "내일",
+"Could not fetch data for \"%s\".<br>Please try another value.<br><a href='%s'>&raquo;&nbsp;settings</a>" => "\"%s\"의 데이터를 가져올 수 없습니다.<br>다른 값을 시도해 보십시오.<br><a href='%s'>&raquo;&nbsp;설정</a>",
+"Dashboard Settings" => "대시보드 설정",
+"<p style=\"padding-top: 20px;\">There are some background colors for the widgets.<ul><li>red: Error</li><li>orange: missconfiguration</li><li>green: there is something new</li><li></li></ul></p>" => "<p style=\"padding-top: 20px;\">위젯의 배경색은 다음을 의미합니다.<ul><li>빨간색: 오류</li><li>주황색: 잘못 설정됨</li><li>녹색: 새로운 항목 사용 가능</li><li></li></ul></p>",
+"From" => "시작",
+"About" => "정보",
+"There are %d new mails..." => "읽지 않은 메일이 %d개 있습니다...",
+"today" => "오늘",
+"tomorrow" => "내일",
+"in two days" => "이틀 안으로",
+"Temperature" => "온도",
+"day" => "낮",
+"min" => "분",
+"max" => "최대",
+"night" => "밤",
+"Wind" => "바람",
+"Speed" => "속도",
+"Direction" => "방향",
+"Cloudiness" => "구름",
+"Pressure" => "기압",
+"Humidity" => "습도",
+"Search" => "검색",
+"Windspeed" => "풍속"
+);
+$PLURAL_FORMS = "nplurals=1; plural=0;";

4e15a5883e1085e2454ba2917e2f82ef6d80a0fd
Pierre Jochem <pierrejochem@msn.com>
2015-02-17 16:38:53 +0100

    Needed changes for OC 8.0
    OC 8.0 needs additional routing entries for calling files via ajax

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -5,3 +5,12 @@ $this->create('ocDashboard_index', '/')->action(
         require __DIR__ . '/../index.php';
     }
 );
+
+$this->create('ocDashboard_appuserconfig', 'ajax/AppUserConfig.php')
+    ->actionInclude('ocDashboard/ajax/AppUserConfig.php');
+
+$this->create('ocDashboard_ajaxservice', 'ajax/ajaxService.php')
+    ->actionInclude('ocDashboard/ajax/ajaxService.php');
+
+$this->create('ocDashboard_reloadwidget', 'ajax/reloadWidget.php')
+    ->actionInclude('ocDashboard/ajax/reloadWidget.php');

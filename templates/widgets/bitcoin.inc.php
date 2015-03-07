52c6d35415dd1dbc314f957e9aa050ca3ef9437c
Florian Steffens <flost@Florians-MBP.fritz.box>
2015-03-07 18:38:05 +0100

    clean

--- templates/widgets/bitcoin.inc.php
+++
@@ -1,4 +0,0 @@
-<?php 
-print_unescaped("<p id='last'>".$additionalparams["last"]." USD</p>"); 
-print_unescaped("<p id='low'>".$additionalparams["low"]." USD</p>"); 
-print_unescaped("<p id='high'>".$additionalparams["high"]." USD</p>"); ?>

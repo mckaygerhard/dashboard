714d36ec69196dd2835f05f6e75200f3d36f0988
Florian Steffens <flost@Florians-MBP.fritz.box>
2015-03-11 19:58:01 +0100

    bookmarks widget ready for oc8

--- widgets/bookmarks/bookmarkscontroller.php
+++ widgets/bookmarks/bookmarkscontroller.php
@@ -62,17 +62,44 @@ class BookmarksController extends WidgetController implements IWidgetController 
     // private services -------------------------------------------------
 
     private function checkDepedencies() {
-        return (App::isEnabled('bookmarks') && class_exists('OC_Bookmarks_Bookmarks'));
+        return (App::isEnabled('bookmarks') &&
+            (class_exists('OC_Bookmarks_Bookmarks') || class_exists('OCA\Bookmarks\Controller\Rest\BookmarkController'))
+        );
     }
 
     /**
      * @return array
      */
     private function getBookmarks() {
+        if( class_exists('OC_Bookmarks_Bookmarks') ) {
+            return $this->getBookmarksOC7();
+        } elseif( class_exists('OCA\Bookmarks\Controller\Rest\BookmarkController')) {
+            return $this->getBookmarksOC8();
+        }
+
+    }
+
+    private function getBookmarksOC7() {
         $filters = $this->getConfig('tagKeyword', 'Dashboard');
         /** @var array $bookmarks */
         /** @noinspection PhpVoidFunctionResultUsedInspection */
         $bookmarks = OC_Bookmarks_Bookmarks::findBookmarks(0, 'clickcount', $filters, true, -1);
         return $bookmarks;
     }
-} 
+
+    private function getBookmarksOC8() {
+        $filters = $this->getConfig('tagKeyword', 'Dashboard');
+        /** @noinspection PhpUnnecessaryFullyQualifiedNameInspection */
+        /** @noinspection PhpUndefinedClassInspection */
+        /** @noinspection PhpUndefinedNamespaceInspection */
+        $bookmarksApp = new \OCA\Bookmarks\AppInfo\Application();
+        if( $bookmarksApp ) {
+            $container = $bookmarksApp->getContainer();
+            $bookmarksController = $container->query('BookmarkController');
+            $bookmarks = $bookmarksController->getBookmarks('bookmark', $filters);
+            $bookmarks = $bookmarks->getData();
+            return $bookmarks['data'];
+        }
+        return;
+    }
+}

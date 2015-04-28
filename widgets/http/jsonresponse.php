10c13749f478c5e411c77239668ca9ffdd97bb5c
Florian Steffens <flost@freans.de>
2015-04-28 13:18:46 +0200

    own json response

---
+++ widgets/http/jsonresponse.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace OCP\AppFramework\Http;
+use OCP\AppFramework\Http;
+
+class JSONResponse extends Response {
+
+protected $data;
+private   $widgetStatus;
+
+public function __construct($data=array(), $statusCode=Http::STATUS_OK) {
+  $this->data = $data;
+  $this->setStatus($statusCode);
+  $this->addHeader('Content-Type', 'application/json; charset=utf-8');
+}
+
+public function render(){
+  return json_encode($this->data);
+}
+
+public function setData($data){
+  $this->data = $data;
+}
+
+public function getData(){
+  return $this->data;
+}
+
+public function setWidgetStatus($data){
+  $this->data = $widgetStatus;
+}
+
+public function getWidgetStatus(){
+  return $this->widgetStatus;
+}
+
+}

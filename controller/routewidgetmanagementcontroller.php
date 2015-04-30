f8cd2c833ef42a907b5e91cfca8ca850dcb3f8a1
Florian Steffens <flost@freans.de>
2015-04-30 11:34:52 +0200

    Update routewidgetmanagementcontroller.php

--- controller/routewidgetmanagementcontroller.php
+++ controller/routewidgetmanagementcontroller.php
@@ -8,9 +8,48 @@
 
 namespace OCA\Dashboard\Controller;
 
-
 use OCP\AppFramework\Controller;
 
 class RouteWidgetManagementController extends Controller {
+  
+  private $widgetManagementService;
+  private $L10N;
+  
+  public function __construct($appName, IRequest $request, $user, IL10N $l10n, WidgetManagementService $widgetManagementService){
+    parent::__construct($appName, $request);
+    $this->user = $user;
+    $this->l10n = $l10n;
+    $this->widgetManagementService = $widgetManagementService;
+  }
 
+  public function getEnabled() {
+    // TODO
+    return array('dummy');
+  }
+  
+  public function getAvailable() {
+    // TODO
+    return array('dummy');
+  }
+  
+  public function enable($wId) {
+    // TODO
+    return true;
+  }
+  
+  public function disable($wId) {
+    // TODO
+    return true;
+  }
+  
+  public function addNewInstance($wId) {
+    // TODO
+    return 'dummy-0';
+  }
+  
+  public function removeInstance($wIId) {
+    // TODO
+    return true;
+  }
+  
 }
